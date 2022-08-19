77d89e6a494479076e46bf2e460742d9995d59ea
wasifekbal <wasifekbal27@gmail.com>
2022-08-19 10:30:28 +0530

    java added..

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,7 +2,7 @@ local treesitter_config = require("nvim-treesitter.configs")
 
 treesitter_config.setup {
   -- ensure_installed = "maintained",
-  ensure_installed = { "c", "cpp", "python", "javascript", "html", "css", "lua", "json"},
+  ensure_installed = { "c", "cpp", "python", "javascript", "html", "css", "lua", "json", "java"},
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
