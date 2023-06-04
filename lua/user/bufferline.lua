6122e818cbc64a47d21d1395aaf5d24ff7509945
wasifekbal <wasifekbal27@gmail.com>
2023-06-04 20:45:36 +0530

    minor icon changes.

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -19,7 +19,7 @@ bufferline_config.setup({
             --[[ style = 'icon' | 'underline' | 'none', ]]
             style = "icon",
         },
-        buffer_close_icon = "",
+        buffer_close_icon = "󱎘",
         modified_icon = "●",
         close_icon = "",
         left_trunc_marker = "",
@@ -76,7 +76,7 @@ bufferline_config.setup({
         color_icons = true, -- whether or not to add the filetype icon highlights
         show_buffer_icons = true, -- disable filetype icons for buffers
         show_buffer_close_icons = true,
-        show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+        --[[ show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon ]]
         show_close_icon = true,
         show_tab_indicators = true,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
