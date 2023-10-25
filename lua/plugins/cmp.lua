f9bd511f1d823f14680fdc997648721efc823006
wasifekbal <wasifekbal27@gmail.com>
2023-10-25 21:39:14 +0530

    added cmp-{buffer,path,cmdline,cmp-nvim-lsp}

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -140,17 +140,22 @@ local function config()
     })
 end
 
-
 return {
     "hrsh7th/nvim-cmp",
     event = "BufReadPre",
     config = config,
     dependencies = {
-        "L3MON4D3/LuaSnip",
-        event = "BufReadPre",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp"
-    }
+        {
+            "L3MON4D3/LuaSnip",
+            event = "BufReadPre",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp",
+        },
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-cmdline",
+    },
 }
