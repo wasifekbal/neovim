0f9d705ec614bfc04c45895dfa357565ce3b4a00
wasifekbal <wasifekbal27@gmail.com>
2026-02-24 02:47:11 +0530

    Migrated nvm-lspconfig to latest format.

--- lua/plugins/nvim-lspconfig/server_setup/init.lua
+++ lua/plugins/nvim-lspconfig/server_setup/init.lua
@@ -1,15 +1,11 @@
-local lspconfig = require("lspconfig")
 local on_attach = require("util.lsp").on_attach
 local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities
+
 local capabilities = cmp_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.offsetEncoding = { "utf-16" }
 
 local function setup_lsp(server, opts)
-    if not lspconfig[server] then
-        vim.notify("LSP server not installed: " .. server, vim.log.levels.WARN)
-        return
-    end
-    lspconfig[server].setup(opts or {})
+    vim.lsp.config(server, opts or {})
 end
 
 -- ==========================
@@ -35,12 +31,10 @@ setup_lsp("lua_ls", {
 
 -- TYPESCRIPT
 local function organize_imports()
-    local params = {
+    vim.lsp.buf.execute_command({
         command = "_typescript.organizeImports",
         arguments = { vim.api.nvim_buf_get_name(0) },
-        title = "",
-    }
-    vim.lsp.buf.execute_command(params)
+    })
 end
 
 setup_lsp("ts_ls", {
@@ -55,7 +49,10 @@ setup_lsp("ts_ls", {
 })
 
 -- GO
-setup_lsp("gopls", {})
+setup_lsp("gopls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- JSON
 setup_lsp("jsonls", {
@@ -79,26 +76,46 @@ setup_lsp("pyright", {
     },
 })
 
--- RUFF (Python linter)
+-- RUFF
 setup_lsp("ruff", {
     on_attach = on_attach,
-    init_options = { settings = { args = {} } },
+    init_options = {
+        settings = { args = {} },
+    },
 })
 
 -- BASH
-setup_lsp("bashls", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("bashls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- HTML
-setup_lsp("html", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("html", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- CSS
-setup_lsp("cssls", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("cssls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- C / C++
-setup_lsp("clangd", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("clangd", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- JAVA
-setup_lsp("jdtls", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("jdtls", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
 
 -- VUE / VOLAR
-setup_lsp("volar", { on_attach = on_attach, capabilities = capabilities })
+setup_lsp("volar", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
